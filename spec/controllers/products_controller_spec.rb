512763c8634bd6d2b4dc29c2974f657fdc12aad1
Kennnedu <r.kenda94@gmail.com>
2019-11-04 16:23:41 +0300

    added "GET index csv pagination" tests and extend "GET index csv"

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,10 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, :type => :controller do
-  describe 'GET index' do
-    it 'has a 200 status code' do
-      get :index, :format => :json
-      expect(response.status).to eq(200)
+  before do
+    @max_products_count = 16
+    @default_limit = 5
+  end
+
+  describe 'GET index csv' do
+    before { get :index, :format => :csv }
+
+    it { expect(response.status).to eq(200) }
+
+    it { expect(response.content_type).to eq('application/octet-stream') }
+
+    it do
+      expect(response.body.split("\n").first).to eq("name,description,price,availability_date,slug,stock_total,category")
+    end
+
+    it { expect(response.body.split("\n").size).to eq(@default_limit + 1) } # with header
+  end
+
+  describe 'GET index csv pagination' do
+    it "has max limit #{@max_products_count}" do
+      get :index, params: { limit: @max_products_count }, format: :csv
+      expect(response.body.split("\n").size).to eq(@max_products_count + 1) # with header
+    end
+
+    it "has max limit #{@max_products_count} and offset one less max limit" do
+      get :index, params: { limit: @max_products_count, offset: (@max_products_count - 1) }, format: :csv
+      expect(response.body.split("\n").size).to eq(2) # with header
+    end
+
+    it "has offset one less max limit" do
+      get :index, params: { offset: (@max_products_count - 1) }, format: :csv
+      expect(response.body.split("\n").size).to eq(2) # with header
     end
   end
 end
